c66ee09d7e55c675a6a22e68b05631217d139688
Suzan <s.sharifna@gmail.com>
2022-11-29 14:39:03 -0500

    added new component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
 
+import TodoRowItem from './components/TodoRowItem';
+
 function App() {
+  const todos = [
+
+    { rowNumber: 1, rowDescription: 'Feed the cat', rowAssigned: 'Jack' },
+    { rowNumber: 2, rowDescription: 'Call the office', rowAssigned: 'Jack' },
+    { rowNumber: 3, rowDescription: 'Make dinner', rowAssigned: 'Eric' },
+    { rowNumber: 4, rowDescription: 'Jogging', rowAssigned: 'Jack' },
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="mt-5 container">
+      <div className="card">
+        <div className="card-header">
+          Your Todo's
+        </div>
+        <div className="card-body">
+          <table class="table table-striped">
+            <thead>
+              <tr className="table-primary">
+                <th scope='col'>#</th>
+                <th scope='col'>Description</th>
+                <th scope='col'>Assigned</th>
+              </tr>
+            </thead>
+            <tbody>
+
+              <TodoRowItem rowNumber={todos[0].rowNumber}
+                rowDescription={todos[0].rowDescription}
+                rowAssigned={todos[0].rowAssigned} />
+
+              <TodoRowItem rowNumber={todos[2].rowNumber}
+                rowDescription={todos[2].rowDescription}
+                rowAssigned={todos[2].rowAssigned} />
+
+              <TodoRowItem rowNumber={todos[1].rowNumber}
+                rowDescription={todos[1].rowDescription}
+                rowAssigned={todos[1].rowAssigned} />
+
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+    </div >
   );
 }
 
